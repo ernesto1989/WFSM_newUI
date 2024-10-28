00f1a05b9c3e686355dea7e6320b36400e9a5d76
Ernesto Cantu <ernesto.cantu@aleph5.com>
2024-10-28 16:59:36 -0600

    Temporary removing render as float in order to W2UI bug report

--- main.js
+++ main.js
@@ -10,3 +10,7 @@
 
 const server = require("./webserver")
 server.initWebProject();
+
+
+//encoded params for looker studio: 
+//'%7B%22ds18.scenario_id%22%3A%22ESC-BASE%22%2C%22ds11.scenario_id%22%3A%22ESC-BASE%22%2C%22ds15.scenario_id%22%3A%22ESC-BASE%22%7D'
