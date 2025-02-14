ca5668b6f1bd40fc031f7f7cca498443eac2d33a
Ernesto Cantu <ernesto.cantu@aleph5.com>
2025-02-13 18:23:30 -0600

    include tabs for showing looker dashboards on simulation

--- main.js
+++ main.js
@@ -14,3 +14,11 @@ server.initWebProject();
 
 //encoded params for looker studio - simulator: 
 //'%7B%22ds0.scenario_id%22%3A%22ESC-BASE%22%2C%22ds1.scenario_id%22%3A%22ESC-BASE%22%7D'
+//%7B%22ds1.scenario_id%22%3A%22ESC-BASE%22%7D
+//%7B%22ds1.scenario_id%22%3A%22Scenario_01%22%7D
+/*
+
+    <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/2becc779-cf30-4a7d-9b37-9363fc1acdb1/page/4wIIE" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+
+    <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/2becc779-cf30-4a7d-9b37-9363fc1acdb1/page/4wIIE?params=%7B%22ds1.scenario_id%22%3A%22Scenario_01%22%7D" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+*/
