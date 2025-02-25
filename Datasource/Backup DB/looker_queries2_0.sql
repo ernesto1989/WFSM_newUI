ebf4e61414e209aa3fd3e42e7859809640263c5d
Ernesto Cantu <ernesto.cantu@aleph5.com>
2025-02-24 22:24:35 -0600

    modifying solutions queries to support city impl

--- Datasource/Backup DB/looker_queries2_0.sql
+++ Datasource/Backup DB/looker_queries2_0.sql
@@ -109,28 +109,28 @@ SELECT
 	END AS NEW_TRL
 FROM(
 	SELECT 
-		z01.scenario_id as SCENARIO,
+		CONCAT(z01.scenario_id,(SELECT CONCAT('_',u01.name) from u01_cities u01 where u01.id = z01.city_id)) as SCENARIO,
 		z01.cdate as DTE,
 		z01.capacity_units as CU,
 		z01.time_units as TU,
-		( select COUNT(*) from a01_nodes a01 where a01.scenario_id = z01.scenario_id) as NODES,
-		( select sum(a01.min_capacity) from a01_nodes a01 where a01.scenario_id = z01.scenario_id) as TOTAL_MIN_VOL,
-		( select sum(a01.current_vol) from a01_nodes a01 where a01.scenario_id = z01.scenario_id) as TOTAL_CURRENT_VOL,
-		( select sum(a01.max_capacity) from a01_nodes a01 where a01.scenario_id = z01.scenario_id) as TOTAL_MAX_VOL,
-		( select sum(a02.current_flow) from a02_flows a02 where a02.scenario_id = z01.scenario_id and a02.origin = 0 ) as CURRENT_INCOMING_FLOW,
-		( select sum(a02.current_flow) from a02_flows a02 where a02.scenario_id = z01.scenario_id and a02.destiny = 0 ) as CURRENT_OUTCOMING_FLOW,
-		( select count(*) from a03_time_to_reach_limit a03 where a03.scenario_id = z01.scenario_id) as SIMULATED,
-		( select min(a03.time_to_reach_limit) from a03_time_to_reach_limit a03 where a03.scenario_id = z01.scenario_id and a03.time_to_reach_limit>=0) as TRL,
-		( select min(a03.time_to_reach_limit) from a03_time_to_reach_limit a03 where a03.scenario_id = z01.scenario_id) as STABLE_TRL,
-		( select count(*) from s01_solution_detail s01 where s01.scenario_id = z01.scenario_id ) as SOLVED,
-		( select min(s01.T) from s01_solution_detail s01 where s01.scenario_id = z01.scenario_id and s01.T >= 0) as NEW_TRL,
-		( select min(s01.T) from s01_solution_detail s01 where s01.scenario_id = z01.scenario_id) as STABLE_NEW_TRL
+		( select COUNT(*) from a01_nodes a01 where a01.scenario_id = z01.scenario_id and a01.city_id = z01.city_id) as NODES,
+		( select sum(a01.min_capacity) from a01_nodes a01 where a01.scenario_id = z01.scenario_id and a01.city_id = z01.city_id) as TOTAL_MIN_VOL,
+		( select sum(a01.current_vol) from a01_nodes a01 where a01.scenario_id = z01.scenario_id and a01.city_id = z01.city_id) as TOTAL_CURRENT_VOL,
+		( select sum(a01.max_capacity) from a01_nodes a01 where a01.scenario_id = z01.scenario_id and a01.city_id = z01.city_id) as TOTAL_MAX_VOL,
+		( select sum(a02.current_flow) from a02_flows a02 where a02.scenario_id = z01.scenario_id and a02.origin = 0 and a02.city_id = z01.city_id ) as CURRENT_INCOMING_FLOW,
+		( select sum(a02.current_flow) from a02_flows a02 where a02.scenario_id = z01.scenario_id and a02.destiny = 0 and a02.city_id = z01.city_id ) as CURRENT_OUTCOMING_FLOW,
+		( select count(*) from a03_time_to_reach_limit a03 where a03.scenario_id = z01.scenario_id and a03.city_id = z01.city_id) as SIMULATED,
+		( select min(a03.time_to_reach_limit) from a03_time_to_reach_limit a03 where a03.scenario_id = z01.scenario_id and a03.time_to_reach_limit>=0 and a03.city_id = z01.city_id) as TRL,
+		( select min(a03.time_to_reach_limit) from a03_time_to_reach_limit a03 where a03.scenario_id = z01.scenario_id and a03.city_id = z01.city_id) as STABLE_TRL,
+		( select count(*) from s01_solution_detail s01 where s01.scenario_id = z01.scenario_id and s01.city_id = z01.city_id) as SOLVED,
+		( select min(s01.T) from s01_solution_detail s01 where s01.scenario_id = z01.scenario_id and s01.T >= 0 and s01.city_id = z01.city_id) as NEW_TRL,
+		( select min(s01.T) from s01_solution_detail s01 where s01.scenario_id = z01.scenario_id and s01.city_id = z01.city_id) as STABLE_NEW_TRL
 	FROM z01_scenarios z01
 ) Q01;
 
 #Solution Details
 select 
-	z01.scenario_id as Scenario,
+	CONCAT(z01.scenario_id,(SELECT CONCAT('_',u01.name) from u01_cities u01 where u01.id = z01.city_id)) as Scenario,
 	a01.node_id as NODE,
 	CONCAT(s01.NActual, ' ', z01.capacity_units) as CURRENT_VOL,
 	CONCAT(s01.E, ' ', z01.capacity_units, '/', z01.time_units) as PROPOSED_INPUT,
@@ -157,5 +157,5 @@ select
 	)
 	 as NEW_TRL
 from s01_solution_detail s01
-join z01_scenarios z01 on z01.scenario_id = s01.scenario_id 
-join a01_nodes a01 on a01.scenario_id = s01.scenario_id and a01.id = s01.`No` 
+join z01_scenarios z01 on z01.scenario_id = s01.scenario_id and z01.city_id = s01.city_id 
+join a01_nodes a01 on a01.scenario_id = s01.scenario_id and a01.id = s01.`No` and a01.city_id = s01.city_id 
