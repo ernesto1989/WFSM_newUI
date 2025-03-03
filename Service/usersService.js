655d0da5bea3d026b1ef47c20972772fa6ae8021
Ernesto Cantu <ernesto.cantu@aleph5.com>
2025-03-03 12:30:03 -0600

    adding bcrypt to session manager

--- Service/usersService.js
+++ Service/usersService.js
@@ -9,6 +9,7 @@
  * 02/24/2025
  */
 const dataSource = require('../Datasource/MySQLMngr');
+const bcrypt = require('bcrypt');
 
 const userQquery = 
 `   SELECT 
@@ -22,7 +23,7 @@ const userQquery =
     from u03_users u03 
     join u02_roles u02 on u02.id = u03.role_id
     left join u01_regions u01 on u01.id = u03.region_id 
-    where u03.username = ? and password = ?
+    where u03.username = ?
 `;
 
 const getUsersQuery = 
@@ -30,7 +31,6 @@ const getUsersQuery =
         u03.id as recid,
         u03.username,
         u03.name,
-        u03.password ,
         u03.role_id, 
         u02.name as role_name,
         u03.region_id,
@@ -44,12 +44,16 @@ const getUsersQuery =
 async function isValidUser(username, password) {
     try{
         let query = userQquery
-        let params = [username,password];
+        let params = [username];
         qResult = await dataSource.getDataWithParams(query,params);
-        return qResult.rows[0];
-        // if (user && user.password === password) {
-        //     return user;
-        // }
+        let user = qResult.rows[0];
+        if(user){
+            let isEqual = await bcrypt.compare(password, user.password);
+            if(isEqual)
+                return user;
+        }
+        return null;
+        
     }catch(err){
         return null;
     }
